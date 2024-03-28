3e47e8f034912fceffcd923fef2cc9d6337eb476
Obua Denis Daniel <dd.obua@gmail.com>
2024-03-28 11:47:06 +0300

    Remove redundant hook import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { StrictMode, useState } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './css/styles.css';
@@ -9,41 +9,3 @@ root.render(
     <App />
   </StrictMode>
 );
-
-// StarRating
-// import StarRating from './components/StarRating';
-
-// const Test = () => {
-//   const [movieRating, setMovieRating] = useState(0);
-
-//   return (
-//     <div>
-//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-//       <p>This movie was rated {movieRating} stars.</p>
-//     </div>
-//   );
-// };
-
-{
-  /* <StarRating maxRating={5} defaultRating={3} />
-    <StarRating
-      color="green"
-      size={36}
-      className="test"
-      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-    />
-    <Test /> */
-}
-
-{
-  /* <StarRating maxRating={10} />
-    <StarRating
-      maxRating={5}
-      // maxRating={5}
-      color="green"
-      size={36}
-      className="test"
-      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
-    />
-    <Test /> */
-}
