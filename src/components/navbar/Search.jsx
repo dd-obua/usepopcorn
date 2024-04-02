96509bc75ff13c6d16c4a0b81f697f84dd5492fd
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-02 16:39:09 +0300

    Focus the search bar when Enter is pressed

--- src/components/navbar/Search.jsx
+++ src/components/navbar/Search.jsx
@@ -2,10 +2,23 @@ import { useEffect, useRef } from 'react';
 
 const Search = ({ query, setQuery }) => {
   const inputElem = useRef(null);
+  const focusInput = () => inputElem.current.focus();
 
   useEffect(() => {
-    inputElem.current.focus();
-  }, []);
+    focusInput();
+
+    const activate = (e) => {
+      if (document.activeElement === inputElem.current) return;
+      if (e.key === 'Enter') {
+        focusInput();
+        setQuery('');
+      }
+    };
+
+    document.addEventListener('keydown', activate);
+
+    return () => document.addEventListener('keydown', activate);
+  }, [setQuery]);
 
   return (
     <input
