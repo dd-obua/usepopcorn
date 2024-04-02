652b3a114a38eabd6113f3fc1cabe55f94a858c6
Obua Denis Daniel <dd.obua@gmail.com>
2024-04-02 12:51:39 +0300

    Store and retireve movies from local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,10 +18,14 @@ export const KEY = '24bc28ba';
 export default function App() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [selectedId, setSelectedId] = useState(null);
+
+  const [watched, setWatched] = useState(() => {
+    const storedValue = localStorage.getItem('watched');
+    return JSON.parse(storedValue);
+  });
 
   const handleSelectMovie = (id) => {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -69,6 +73,10 @@ export default function App() {
 
     return () => controller.abort();
   }, [query]);
+
+  useEffect(() => {
+    localStorage.setItem('watched', JSON.stringify(watched));
+  }, [watched]);
 
   return (
     <>
